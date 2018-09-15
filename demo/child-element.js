ab8a6bce7e73f01f452129160053b8eeeb28899c
Scott <slindeneau@rosey.io>
2018-09-14 17:02:02 -0700

    fixups

--- demo/child-element.js
+++ demo/child-element.js
@@ -8,28 +8,19 @@ class ChildElement extends MutableData(PolymerElement) {
             arrayData:{
               type:Array,
               notify:true
-            },
-            objectData:{
-              type: Object
             }
         };
     }
     static get observers() {
       return [
-        '_arrayObserver(arrayData.*)',
-        '_objectObserver(objectData.*)'
+        '_arrayObserver(arrayData.*)'
       ]
     }
   _arrayObserver(thingA){
     console.log("Array observer called",thingA);
   }
   connectedCallback(){
-    this.set('arrayData',[]);
+    this.set('arrayData',['uid',Math.random().toString(36).substring(7)]);
   }
-  // _objectObserver(thingB){
-  //   console.log("Object observer called",thingB);
-  //   this.set('arrayData',[]);
-  //   console.log("Object Observer done");
-  // }
 }
 customElements.define('child-element', ChildElement);
